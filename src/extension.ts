c470930b7f82ba9815acba26e6f5a69db7e8f61c
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-07 12:49:01 +0800

    feat: listen to file change event and update diagnostic (#5)
    feat: lisent to file change event and update diagnostic

--- src/extension.ts
+++ src/extension.ts
@@ -104,39 +104,11 @@ export async function activate(context: vscode.ExtensionContext) {
     })
   );
 
-  // const configWatcher = vscode.workspace.createFileSystemWatcher(
-  //   "**/*.{md,mdx}",
-  //   true,
-  //   false,
-  //   true
-  // );
-
-  // mdWatcher.onDidChange(async (uri) => {
-  //   const readFile = await vscode.workspace.fs.readFile(uri);
-  //   const readStr = Buffer.from(readFile).toString("utf8");
-  //   const pathArr = uri.path.split("/");
-  //   const fileNameArr = pathArr[pathArr.length - 1].split(".");
-  //   const fileExtension = fileNameArr[fileNameArr.length - 1];
-
-  //   if (fileExtension === "md") {
-  //     const { data } = matter(readStr);
-  //     if ("type" in data) {
-  //       console.log("hi type", data.type);
-  //     } else {
-  //       // if strict === true, we need to alarm that frontmatter doesn't have type key
-  //       if (config.strict) {
-
-  //       }
-  //     }
-  //   }
-  //   // Recognize md and mdx
-  //   // md -> safely utilize frontmatter
-  //   // mdx -> make sure we can handle export prop and frontmatter at the same time
-
-  //   console.log(config);
-  // });
-
-  // context.subscriptions.push(mdWatcher);
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument((event) => {
+      updateDiagnostics(event.document, collection, config);
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
