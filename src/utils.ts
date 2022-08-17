128ac8bd3217e8eb044664f85956e7c9268c125f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-17 08:59:19 +0800

    feat: add better getKeyPosition

--- src/utils.ts
+++ src/utils.ts
@@ -1,53 +1,30 @@
 import * as vscode from "vscode";
 
-const getKeyPosition = (document: vscode.TextDocument, key: string) => {
-  for (let i = 0; i < document.lineCount; i++) {
-    const line = document.lineAt(i);
-    if (line.text.includes(key)) {
-      const start = line.text.indexOf(key);
-      const end = line.text.indexOf(key) + key.length;
+export const getKeyPosition = (document: vscode.TextDocument, key: string) => {
+  let pattern = new RegExp(`${key}:`);
+  const match = pattern.exec(document.getText());
 
-      // We need to find the right key
-
-      if (start === 0) {
-        return {
-          start,
-          end,
-          line: i,
-        };
-      } else {
-        // We have to make sure the child's key is at the first position of the line too
-        // {
-        //   parent: {
-        //     child: "hi" <--- If this is the right key, it won't have any character before itself
-        //   }
-        // }
-        const textList = line.text.split(" ").filter((e) => e !== "");
-        if (textList[0] === key + ":") {
-          return {
-            start,
-            end,
-            line: i,
-          };
-        }
-      }
-    }
+  if (match) {
+    return {
+      start: document.positionAt(match.index),
+      end: document.positionAt(match.index + match[0].length),
+    };
+  } else {
+    return {
+      start: new vscode.Position(0, 0),
+      end: new vscode.Position(0, 1),
+    };
   }
-  return {
-    start: 0,
-    end: 0,
-    line: 0,
-  };
 };
 
-const getFileExtension = (document: vscode.TextDocument): string => {
+export const getFileExtension = (document: vscode.TextDocument): string => {
   const pathArr = document.uri.path.split("/");
   const fileNameArr = pathArr[pathArr.length - 1].split(".");
   const fileExtension = fileNameArr[fileNameArr.length - 1];
   return fileExtension;
 };
 
-const sortedWorkspaceFolders = (): string[] => {
+export const sortedWorkspaceFolders = (): string[] => {
   let _sortedWorkspaceFolders: string[] | undefined;
 
   if (_sortedWorkspaceFolders === void 0) {
@@ -67,5 +44,3 @@ const sortedWorkspaceFolders = (): string[] => {
   }
   return _sortedWorkspaceFolders;
 };
-
-export { getKeyPosition, getFileExtension, sortedWorkspaceFolders };
