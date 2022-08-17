54e01d46de642b9a5778d6228879ef21f5824efd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-17 11:06:15 +0800

    feat: support nested object validation

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,61 @@
 import { YAMLException } from "js-yaml";
 import * as vscode from "vscode";
 
-export const getKeyPosition = (document: vscode.TextDocument, key: string) => {
-  let pattern = new RegExp(`${key}:`);
-  const match = pattern.exec(document.getText());
+/**
+ * Use startLine to determine whether the match text is the right one.
+ * But the caveat is this only working at searching simple or object structure.
+ *
+ * In the situation below, we will recursively validate the object, everytime the position
+ * go deeper one level, it needs to send the updated startPosition (In this case, the initial
+ * startPosition will be {position: 0, offset: 0}, updated startPosition will be {position: 2, offset: 0})
+ *
+ * ```
+ * ---
+ * id: "getKey",
+ * test: {
+ *   id: "another key"
+ * }
+ * ---
+ * ```
+ */
 
-  if (match) {
+export type getKeyPositionProps = {
+  document: vscode.TextDocument;
+  key: string;
+  startPosition: {
+    line: number;
+    offset: number;
+  };
+};
+
+export const getKeyPosition = ({
+  document,
+  key,
+  startPosition,
+}: getKeyPositionProps) => {
+  let pattern = new RegExp(`${key}:`, "g");
+  let match;
+  let target;
+
+  while ((match = pattern.exec(document.getText())) !== null) {
+    const start = document.positionAt(match.index);
+
+    if (start.line > startPosition.line) {
+      target = match;
+      break;
+    } else if (start.line === startPosition.line) {
+      // situation about key and value are in the same line { key: { foo: "h1", bar: "yo" }}
+      if (start.character > startPosition.offset) {
+        target = match;
+        break;
+      }
+    }
+  }
+
+  if (target) {
     return {
-      start: document.positionAt(match.index),
-      end: document.positionAt(match.index + match[0].length),
+      start: document.positionAt(target.index),
+      end: document.positionAt(target.index + target[0].length),
     };
   } else {
     return {
